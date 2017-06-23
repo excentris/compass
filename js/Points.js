6d8574a9c138c4c4efed2a28e711a6f8a192f962
Nick Houghton <nhoughto5@gmail.com>
2017-06-23 14:08:07 -0700

    Have a circle drawing on the canvas

--- js/Points.js
+++ js/Points.js
@@ -1,38 +1,48 @@
 /**
  * Created by nickhoughton on 6/21/17.
  */
-var points = [];
 
 class Points extends Animation{
   constructor(ctx, can){
     super(ctx, can);
+    //document.addEventListener("mousemove", this.mouseDownHandler);
+    $(document).bind("mousemove", this.mouseDownHandler );
   }
 
   getVertexShader(){
     return `
-      attribute vec3 _xy;
-      void main(){
-        gl_Position = vec4(_xy, 1.0);
-        gl_PointSize = 20.0;
+      attribute vec4 aPosition;
+      
+      void main() {
+        gl_Position = aPosition;
       }
     `;
   }
 
   getFragmentShader(){
     return `
-      precision mediump float;
-      void main(){
-        float d = distance(gl_PointCoord, vec2(0.5, 0.5));
-        if(d < 0.5){
-          gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-        }
-        else{
-          discard;
-        }
+      void main() {
+        gl_FragColor = vec4(0.0, 1.0, 1.0, 1.0);
       }
     `;
   }
 
+  circleVertices(){
+    let vertices = [], vertCount = 2, vertexBuffer = this.glContext.createBuffer();
+    for(let i = 0.0; i <= 360; ++i) {
+      let j = i * Math.PI / 180;
+      vertices = vertices.concat([Math.sin(j), Math.cos(j)]);
+      vertices = vertices.concat([0, 0]);
+    }
+
+    this.glContext.bindBuffer(this.glContext.ARRAY_BUFFER, vertexBuffer);
+    this.glContext.bufferData(this.glContext.ARRAY_BUFFER, new Float32Array(vertices), this.glContext.STATIC_DRAW);
+    let vPosition = this.glContext.getAttribLocation(this.shaderProgram, "aPosition");
+    this.glContext.enableVertexAttribArray(vPosition);
+    this.glContext.vertexAttribPointer(vPosition, vertCount, this.glContext.FLOAT, false, 0, 0);
+
+    return vertices.length / vertCount;
+  }
   initShader(){
     let vertexShader = this.getShader(this.getVertexShader(), this.glContext.VERTEX_SHADER, "VERTEX");
     let fragmentShader = this.getShader(this.getFragmentShader(), this.glContext.FRAGMENT_SHADER, "FRAGMENT");
@@ -40,34 +50,26 @@ class Points extends Animation{
     this.glContext.attachShader(this.shaderProgram, vertexShader);
     this.glContext.attachShader(this.shaderProgram, fragmentShader);
     this.glContext.linkProgram(this.shaderProgram);
+    if (!this.glContext.getProgramParameter(this.shaderProgram, this.glContext.LINK_STATUS)) {
+      console.log('Unable to initialize the shader program: ' + this.glContext.getProgramInfoLog(this.shaderProgram));
+    }
+    this.glContext.useProgram(this.shaderProgram);
   }
+
   mouseDownHandler(e, xyz){
-    let xClick = e.clientX;
-    let yClick = e.clientY;
-
-    let x = (xClick / this.CANVAS.width);
-    let y = (yClick / this.CANVAS.height);
-
-    points.push([x,y]);
-
-    for(var i = 0; i < points.length; i++){
-      this.glContext.vertexAttrib3f(xyz, points[i][0], points[i][1], 0.0);
-      this.glContext.drawArrays(this.glContext.POINTS, 0, 1);
-    }
+    console.log("MouseDown");
   }
 
-  drawCircle(x, y, xy){
-    points.push([x,y]);
-
-    for(var i = 0; i < points.length; i++){
-      this.glContext.vertexAttrib3f(xy, points[i][0], points[i][1], 0.0);
-      this.glContext.drawArrays(this.glContext.POINTS, 0, 1);
-    }
-  }
 
   render(){
     this.initShader();
-    let xy = this.glContext.getAttribLocation(this.shaderProgram, "_xy");
-    this.drawCircle(0.5, 0.5, xy);
+    let n = this.circleVertices();
+    if(n < 0){
+      console.log("Failed to create circle");
+      return;
+    }
+    this.glContext.clearColor(241/255,238/255,217/255,1);
+    this.glContext.clear(this.glContext.COLOR_BUFFER_BIT);
+    this.glContext.drawArrays(this.glContext.TRIANGLE_STRIP, 0, n);
   }
 }
