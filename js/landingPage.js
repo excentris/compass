145e347b9c99b5bef9d20791a63a55ba30ed8a64
Nick Houghton <nhoughto5@gmail.com>
2017-07-21 09:35:37 -0700

    Got cloud circling page

--- js/landingPage.js
+++ js/landingPage.js
@@ -11,17 +11,45 @@ function main() {
   game.state.start('mainState');
 }
 
+function randomFloat(min, max, pre){
+  if(pre === 0){
+    return Math.round(Math.random() * (min - max) + max);
+  }else{
+  return (Math.random() * (min - max) + max).toFixed(pre || 2);
+  }
+}
+
 var mainState = {
   preload: function(){
     game.load.image('cloud', '/images/coud1.png');
   },
   create: function(){
-    cloudField = game.add.tileSprite(0,0,size[0], size[1], 'cloud');
-    // cloudField.scale.x = 0.25;
-    // cloudField.scale.y = 0.25;
+    // let clouds = [];
+    // for(let i = 0; i < 5; ++i){
+    //   clouds[i] = game.add.sprite(randomFloat(size[0], size[1]), randomFloat(size[0], size[1]), 'cloud');
+    //   game.physics.enable(clouds[i], Phaser.Physics.ARCADE);
+    //   clouds[i].body.velocity.x = randomFloat(45, 55);
+    //   clouds[i].scale.setTo(randomFloat(0.1, 0.4), randomFloat(0.1, 0.4));
+    // }
+
+    var x = randomFloat(size[0], size[1], 0);
+    var y = randomFloat(0, game.height, 0);
+    var dx = randomFloat(45, 55, 0);
+    var sx = randomFloat(0.1, 0.4, 2);
+    var sy = randomFloat(0.1, 0.4, 2)
+    cloudField = game.add.sprite(x, y, 'cloud');
+    cloudField.position.y = y;
+    cloudField.position.x = 0;
+    game.physics.enable(cloudField, Phaser.Physics.ARCADE);
+    cloudField.body.velocity.x = dx;
+    //cloudField.scale.setTo(sx, sy);
+
   },
   update:function(){
-    cloudField.tilePosition.x += 1;
+    //console.log("Cloud Pos: " + cloudField.position.x + "   " + game.width + "      " + cloudField.width);
+    if(cloudField.position.x > game.width){
+      cloudField.position.x = -cloudField.width;
+    }
   }
 }
 
