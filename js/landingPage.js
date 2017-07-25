8d998620773e6a8a7ae7c038abe34ddabaa2e731
Nick Houghton <nhoughto5@gmail.com>
2017-07-24 21:52:45 -0700

    Have night and day working as well as transition

--- js/landingPage.js
+++ js/landingPage.js
@@ -2,29 +2,27 @@
 * Created by nickhoughton on 6/20/17.
 */
 
-var cloudField;
 var game, size;
-var clouds = [], cloudNames, numClouds = 5, sun, day = true;
+var clouds = [], numClouds = 5, sun, day = true, lastTime = 0, timePattern = 10000, transitionTime = 5000;
 var numCloudImages = 9;
 function main() {
   $("night").css('opacity', 1.0);
   size = getViewport();
-  game = new Phaser.Game(size[0], size[1], Phaser.CANVAS, 'body', null, true);
+  game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.CANVAS, 'body', null, true);
   game.state.add('mainState', mainState);
   game.state.start('mainState');
+  requestAnimationFrame(animate);
 }
 
 function addCloud(i, n){
-  var x = randomFloat(-game.width, game.width, 0);
-  var y = randomFloat(0, game.height, 0);
-  var dx = randomFloat(45, 55, 0);
+  // var x = randomFloat(-game.width, game.width, 0);
+  // var y = randomFloat(0, game.height, 0);
   var sx = randomFloat(0.1, 0.4, 2);
-  //var sy = randomFloat(0.1, 0.4, 2);
   clouds[i] = game.add.sprite(0, 0, 'cloud' + n);
-  clouds[i].position.y = y;
-  clouds[i].position.x = x;
+  clouds[i].position.y = randomFloat(0, game.height, 0);
+  clouds[i].position.x = randomFloat(-game.width, game.width, 0);
   game.physics.enable(clouds[i], Phaser.Physics.ARCADE);
-  clouds[i].body.velocity.x = dx;
+  clouds[i].body.velocity.x = randomFloat(45, 55, 0);
   clouds[i].body.velocity.y = randomFloat(-5, 5, 0);
   clouds[i].scale.setTo(sx, sx);
   clouds[i].anchor.setTo(0.5,0.5);
@@ -44,6 +42,7 @@ function addSun(){
 var mainState = {
   preload: function(){
     game.stage.disableVisibilityChange = true;
+    game.canvas.className += "day";
     for(let i = 1; i <= numCloudImages; ++i){
       let n = 'cloud' + i;
       game.load.image(n, '/images/' + n + '.png');
@@ -69,33 +68,52 @@ var mainState = {
         clouds[i].body.velocity.y = randomFloat(-5, 5, 0);
       }
     }
+    if((sun.position.x > (game.width + sun.width)) || (sun.position.y > (game.height + sun.height))){
+      sun.position.x = -sun.width;
+      sun.position.y = randomFloat(sun.height, (game.height - sun.height),0);
+      sun.body.velocity.x = randomFloat(45, 60, 0);
+      sun.body.velocity.y = randomFloat(-7, 7, 0);
+    }
   }
 }
 
 function fadeNightOut(){
-  $("#night").fadeTo(4200, 0, function() {
+  $("#night").fadeTo(transitionTime, 0, function() {
     // Animation complete.
   });
   $("#wrapper").animate({
     color: '#46433A'
-  }, 4200);
+  }, transitionTime);
 }
 function fadeNightIn(){
-  $("#night").fadeTo(4200, 1, function() {
+  $("#night").fadeTo(transitionTime, 1, function() {
     // Animation complete.
   });
   $("#wrapper").animate({
     color: 'rgb(144, 138, 120)'
-  }, 4200);
+  }, transitionTime);
 }
 function toggleDayNight(){
-  // if(day) {
-  //   fadeNightIn();
-  // }else{
-  //   fadeNightOut()
-  // }
-  // day = !day;
-  $("body").toggleClass("day night", 4200);
+  if(day) {
+    fadeNightIn();
+  }else{
+    fadeNightOut()
+  }
+  day = !day;
 }
+function animate(currentTime){
+  if(day && $(night).css('opacity') == 1){
+    lastTime = currentTime;
+  }
+  if(!day && $(night).css('opacity') == 0){
+    lastTime = currentTime;
+  }
+  if(currentTime >= lastTime + timePattern){
+    toggleDayNight();
+    lastTime = currentTime;
+  }
+  requestAnimationFrame(animate);
+}
+
 window.onload = main;
 
